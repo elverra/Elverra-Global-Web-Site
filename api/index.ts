717cb5f2c367115201fb90ce766299144862051a
Ephraim D Oladokun <oladokunefi123@gmail.com>
2025-09-07 21:45:00 +0000

    ii

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 // Vercel serverless function entry point
 import express from 'express';
-import { registerRoutes } from '../server/routes.ts';
+import { registerRoutes } from '../server/routes';
 
 const app = express();
 app.use(express.json());
