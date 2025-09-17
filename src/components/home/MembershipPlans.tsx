809a76b7d5f25210483e2a8fd05770f975761658
Ephraim D Oladokun <oladokunefi123@gmail.com>
2025-09-17 22:08:25 +0000

    fix: rename Carte Kiddies plan to Kiddies Card for consistency

--- src/components/home/MembershipPlans.tsx
+++ src/components/home/MembershipPlans.tsx
@@ -31,7 +31,7 @@ const MembershipPlans = () => {
 
   const plans = [
     {
-      name: "Carte Kiddies",
+      name: "Kiddies Card",
       price: "5,000",
       monthly: "500",
       discount: "100%",
